8b8834077b927ef44c65c72781690fe9e5e2f29b
bahaarch <bahaa13pix@gmail.com>
2025-04-23 21:04:24 +0100

    Update in.sh

--- in.sh
+++ in.sh
@@ -3,5 +3,6 @@ curl -sO https://bahaarch.github.io/2part.sh
 curl -sO https://bahaarch.github.io/3install.sh
 curl -sO https://bahaarch.github.io/4configs.sh
 curl -sO https://bahaarch.github.io/5finishing.sh
+curl -sO https://bahaarch.github.io/test.sh
 chmod +x *.sh
-sh 1setup.sh
+sh test.sh
