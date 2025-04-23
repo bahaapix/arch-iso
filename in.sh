f04dc7b48776907bc4b8876b92a946b914ccb684
bahaarch <bahaa13pix@gmail.com>
2025-04-23 20:59:42 +0100

    Add files via upload

---
+++ in.sh
@@ -0,0 +1,6 @@
+curl -sO https://bahaarch.github.io/1setup.sh
+curl -sO https://bahaarch.github.io/2part.sh
+curl -sO https://bahaarch.github.io/3install.sh
+curl -sO https://bahaarch.github.io/4configs.sh
+curl -sO https://bahaarch.github.io/5finishing.sh
+chmod +x *.sh
