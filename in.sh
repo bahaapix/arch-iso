9f063faf61837258a87fdc5ad0f835daa0365f41
bahaarch <bahaa13pix@gmail.com>
2025-04-23 21:05:53 +0100

    Delete in.sh

--- in.sh
+++
@@ -1,8 +0,0 @@
-curl -sO https://bahaarch.github.io/1setup.sh
-curl -sO https://bahaarch.github.io/2part.sh
-curl -sO https://bahaarch.github.io/3install.sh
-curl -sO https://bahaarch.github.io/4configs.sh
-curl -sO https://bahaarch.github.io/5finishing.sh
-curl -sO https://bahaarch.github.io/test.sh
-chmod +x *.sh
-sh test.sh
